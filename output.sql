6ae4aa91e1a1436734dceb60a2bcccc8d6c6ef16
samruddhinaik <samruddhinaik.1995@gmail.com>
2023-02-28 23:40:00 +0000

    Added the address SQL file

---
+++ output.sql
@@ -0,0 +1,189 @@
+ /* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS TABLE_NAME(
+Emp_ID INT(11),
+Address_ID INT(11),
+Address_1 INT(11),
+Address_2 VARCHAR( 100 ),
+Address_3 VARCHAR( 100 ),
+County VARCHAR( 100 ),
+Country VARCHAR( 100 ),
+Postcode VARCHAR( 100 ),
+Date_From VARCHAR( 100 ),
+Date_To INT(11),
+Cohabiting INT(11)
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    24,1,NULL,'Church Farm','Norton Bavant','Wiltshire','England','BA12 0EN','2010-08-01',NULL,4
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    1,2,4,'Main Road','Codford','Wiltshire','UK','BA12 0NJ','2018-01-23',NULL,2
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    21,3,75,'High Street','Keynsham','Somerset','UK','BS31 2NB','   2016-01-01',NULL,1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    20,4,3,'Station Rd','Keynsham','Somerset','UK','BS31 1FF','2017-01-23',NULL,3
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    4,5,126,'Park Road','Heytesbury','Wiltshire','England','BA12 0EG','2014-09-01',2016-07-01,4
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    30,6,52,'London Road','Keynsham','Somerset',NULL,'BS31 1FF','2016-06-01',NULL,3
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    7,7,9,'Victoria Rd.','Norton Bavant','Wiltshire','UK','BA12 0EN','2016-04-11',NULL,3
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    27,8,4,'Church Street','Keynsham','Somerset','UK','BS31 2NB',NULL,NULL,4
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    18,9,23,' Manor Road','Keynsham','Somerset','UK','BS31 1FF',NULL,2016-03-01,4
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    11,10,4,'The Avenue','Keynsham','somerset','England','BS31 1FF','2016-06-01',NULL,1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    31,11,24,'Park Lane','Keynsham','Somerset','UK','BS31 1FF','2016-03-01',NULL,NULL
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    17,12,5,'Queens Sq','Codford','Wiltshire',NULL,'BA12 0NJ','2016-05-10',NULL,2
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    5,14,76,'The Crescent','Codford','Wiltshire','UK','BA12 0NJ','2017-01-23',NULL,3
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    12,15,23,'High Street','Keynsham','Somerset','UK','BS31 1FF','2018-01-27',NULL,1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    22,16,4,'Station Road','Heytesbury','Wiltshire','England','BA12 0EG','2016-04-01',NULL,3
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    4,17,109,'Park Green','Norton Bavant','Wiltshire','England','BA12 0EN','2016-07-01',NULL,1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    14,18,NULL,'Glebe Farm','Keynsham','Somerset','UK','BS31 2NB',NULL,NULL,2
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    2,19,45,'Winsor Rd','Codford','Willtshire',NULL,'BA12 0NJ','2016-02-01',NULL,2
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    6,20,23,'Mill Lane','Keynsham','Somerset','England',NULL,'2013-07-01',NULL,3
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    29,21,34,'Windsor Lane','Codford','Wiltshire','UK','BA12 0NJ','2016-08-01',NULL,0
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    1,22,12,'Main road','Codford','Wiltshire','England','BA12 0NJ','2016-09-01',NULL,0
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    15,23,34,'Exeter Road','Heytesbury','Wiltshire','UK','BA12 0EG','2016-02-31',NULL,4
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    28,24,13,'Parkway','Codford','Wiltshire','UK','BA12 0NJ','2014-06-01',NULL,NULL
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    10,25,21,'Kingsway','Keynsham','Somerset','UK','BS31 2NB','2016-08-01',NULL,4
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( Emp_ID,Address_ID,Address_1,Address_2,Address_3,County,Country,Postcode,Date_From,Date_To,Cohabiting )
+VALUES
+(
+    13,26,45,'Bristol Road','Keynsham','Somerset','UK','BS31 1FF','2016-07-21',NULL,1
+);
